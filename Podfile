45f1a48de55f42115d8887392e3e2b85da2bdcb7
Bojan Markovic <bolom992@gmail.com>
2019-07-13 02:29:13 +0200

    Initial commit.

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/iBolom/cocoapods-spec.git'
+
+target 'TestPods' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'TestPodFramework'
+
+  # Pods for TestPods
+
+end
---
+++ Pods/TestPodFramework/TestPodFramework/TestPodFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestPodFramework.h
+//  TestPodFramework
+//
+//  Created by Bojan Markovic on 13/07/2019.
+//  Copyright © 2019 Bojan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPodFramework.
+FOUNDATION_EXPORT double TestPodFrameworkVersionNumber;
+
+//! Project version string for TestPodFramework.
+FOUNDATION_EXPORT const unsigned char TestPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodFramework/PublicHeader.h>
+
+
